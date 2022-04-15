df09ae87d443340e6e75785443017cea2a39842b
Yuvraj-Ganshi <85454673+yuvraj0651@users.noreply.github.com>
2022-04-15 13:14:50 +0530

    Add files via upload

---
+++ greatesttwonumbers.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter your first number :"))
+num2 = int(input("Enter your second number :"))
+
+if num1 >= num2:
+    if num1 == num2:
+        print("Both the numbers are equal to each other")
+    else:
+        print("number one is greater than number two")
+
+else:
+    print("number two is greater than number one")
